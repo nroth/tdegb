6cb04d3b966b3b9aa1df09954d6feb7ff068640d
Nathaniel Roth <nathanielroth1@gmail.com>
2020-01-14 22:31:41 +0900

    More checkpointing work in progress

--- main_test_gsl_ntuple_histogram_2d.cpp
+++ main_test_gsl_ntuple_histogram_2d.cpp
@@ -18,7 +18,7 @@ struct ntuple_data
   {
 
     // m_r, z, mbh, mstar
-    double attributes[2];
+    double attributes[3];
 
     /*
     double m_r;
@@ -63,8 +63,6 @@ int main(int argc, char **argv)
   double mean_m_r = min_m_r + 0.5 * (max_m_r - min_m_r);
   double sigma_m_r = 0.2 * (max_m_r - min_m_r);
 
-
-
   
   double min_z = 0;
   double max_z = 0.2;
@@ -93,10 +91,10 @@ int main(int argc, char **argv)
   int num_samples = 10000000;
 
   clock_t begin;
+  clock_t end;
   
 
   gsl_ntuple *gal_ntuple  = gsl_ntuple_create(filename_array, &gal_row, sizeof (gal_row));
-
 
 
   begin = clock();
@@ -105,8 +103,8 @@ int main(int argc, char **argv)
 
       gal_row.attributes[0] = mean_m_r + gsl_ran_gaussian(rangen, sigma_m_r);
       gal_row.attributes[1] = mean_z + gsl_ran_gaussian(rangen, sigma_z);
+      gal_row.attributes[2] = 6.;
 
-      //gal_row.mbh = 6.;
       //      gal_row.mstar = 8.;
 
       gsl_ntuple_write(gal_ntuple);
@@ -117,10 +115,12 @@ int main(int argc, char **argv)
 
 
   
-  clock_t end = clock();
+  end = clock();
   float elapsed_secs = float(end - begin) / CLOCKS_PER_SEC;
   printf("#\n# It took %f seconds to generate %d samples\n",elapsed_secs, num_samples);
 
+  struct bin_param_1d {gsl_histogram * hist; int icol;};
+  struct bin_param_2d {gsl_histogram * hist; gsl_histogram * hist_v; int icol; int icol_v; int ibin;};
 
   // create 1d histogram
   begin = clock();
@@ -128,9 +128,7 @@ int main(int argc, char **argv)
   S.function = &sel_func_1d;
   V.function = &val_func_1d;
 
-  struct bin_param {gsl_histogram * hist; int icol;};
-
-  bin_param bin_params = {hist_gals_m_r, 0};
+  bin_param_1d bin_params = {hist_gals_m_r, 0};
   V.params = &bin_params;
 
   gal_ntuple = gsl_ntuple_open (filename_array, &gal_row, sizeof (gal_row));
@@ -140,6 +138,28 @@ int main(int argc, char **argv)
   string x_name("m_r");
   string outfilename = "gsl_hist_1d_" + x_name + ".hist";
   Print_Hist1d(hist_gals_m_r, outfilename);
+
+  end = clock();
+  elapsed_secs = float(end - begin) / CLOCKS_PER_SEC;
+  printf("#\n# It took %f seconds to project to 1d histogram\n",elapsed_secs);
+
+  // create 1d histogram
+  begin = clock();
+
+  //  S.function = &sel_func_1d;
+  //  V.function = &val_func_1d;
+
+  bin_params.hist = hist_gals_z;
+  bin_params.icol = 1;
+  V.params = &bin_params;
+
+  gal_ntuple = gsl_ntuple_open (filename_array, &gal_row, sizeof (gal_row));
+  gsl_ntuple_project(hist_gals_z,gal_ntuple,&V,&S);
+  gsl_ntuple_close(gal_ntuple);
+
+  x_name = "z";
+  outfilename = "gsl_hist_1d_" + x_name + ".hist";
+  Print_Hist1d(hist_gals_z, outfilename);
 
   end = clock();
   elapsed_secs = float(end - begin) / CLOCKS_PER_SEC;
@@ -169,22 +189,22 @@ int main(int argc, char **argv)
   fclose(outfile);
 
 
-  struct hist_param {gsl_histogram * hist;};
-  //bin param already defined
-  
-  hist_param h_params = {hist_gals_m_r};
-  V.params = &h_params;
-
   S.function = &sel_func_2d;
   V.function = &val_func_2d;
 
 
+  int i_m_r = 0;
+  int i_z = 1;
+  bin_param_2d bin_params_2_V = {hist_gals_m_r,hist_gals_z, i_m_r,i_z, 0};
+  V.params = &bin_params_2_V;
+
+  bin_param_2d bin_params_2_S = {hist_gals_m_r,hist_gals_z, i_m_r,i_z, 0}; // just starting definition
 
   for (int iz = 0; iz < num_bins_z; iz++)
     {
 
-      bin_param b_params = {hist_gals_z, iz};
-      S.params = &b_params;
+      bin_params_2_S.ibin = iz;
+      S.params = &bin_params_2_S;
 
       gal_ntuple = gsl_ntuple_open (filename_array, &gal_row, sizeof (gal_row));
   
@@ -202,15 +222,6 @@ int main(int argc, char **argv)
   end = clock();
   elapsed_secs = float(end - begin) / CLOCKS_PER_SEC;
   printf("#\n# It took %f seconds to project to histograms\n",elapsed_secs);
-
-
-  begin = clock();
-  gal_ntuple = gsl_ntuple_open (filename_array, &gal_row, sizeof (gal_row));
-  gsl_ntuple_close(gal_ntuple);
-  end = clock();
-  elapsed_secs = float(end - begin) / CLOCKS_PER_SEC;
-  printf("#\n# It took %f seconds just to open and close the n-tuple\n",elapsed_secs);
-
 
   begin = clock();
   gal_ntuple = gsl_ntuple_open (filename_array, &gal_row, sizeof (gal_row));
@@ -266,84 +277,45 @@ int sel_func_1d (void *this_data, void *p)
 int sel_func_2d (void *this_data, void *p)
 {
 
-  struct bin_struct { gsl_histogram * hist; int ibin; };
+  struct bin_struct { gsl_histogram * hist; gsl_histogram * hist_v; int icol; int icol_v; int ibin_v;};
   bin_struct binfo = *(struct bin_struct *)p;
 
-  gsl_histogram * vertical_hist = binfo.hist;
-  int vertical_bin_index = binfo.ibin;
-  
+  gsl_histogram * vertical_hist = binfo.hist_v;
 
   struct ntuple_data * data = (struct ntuple_data *) this_data;
-  double m_r, z, mbh,mstar;
-  //  scale = *(double *) params;
-
-  m_r = data->attributes[0];
-  z = data->attributes[1];
-  //  mbh = data -> mbh;
-  //  mstar = data -> mstar;
+  double this_vertical_data = data->attributes[binfo.icol_v];
 
 
   // need to replace z with generic vertical variable
-  if (z >= gsl_histogram_max(vertical_hist))
+  if (this_vertical_data >= gsl_histogram_max(vertical_hist))
     {
-      z = (1. - 1.e-15) * gsl_histogram_max(vertical_hist);
+      this_vertical_data = (1. - 1.e-15) * gsl_histogram_max(vertical_hist);
     }
-  if (z < gsl_histogram_min(vertical_hist))
+  if (this_vertical_data < gsl_histogram_min(vertical_hist))
     {
-      z = gsl_histogram_min(vertical_hist);
+      this_vertical_data = gsl_histogram_min(vertical_hist);
     }
 
-  return ( z >= vertical_hist->range[vertical_bin_index] &&  z < vertical_hist->range[vertical_bin_index + 1]);
+  return ( this_vertical_data >= vertical_hist->range[binfo.ibin_v] && this_vertical_data < vertical_hist->range[binfo.ibin_v + 1]);
   //  return ( z >= 0.1);
   // return true;
 }
 
-double val_func_2d (void *this_data, void *p)
-{
-  //  (void)(params); /* avoid unused parameter warning */
-
-  struct hist_struct { gsl_histogram * hist; };
-
-  
-  struct ntuple_data * data = (struct ntuple_data *) this_data;
-  double m_r, z,mbh,mstar;
-
-  hist_struct ph = *(struct hist_struct *)p;
-
-  m_r = data->attributes[0];
-  z = data->attributes[1];
-  //  mbh = data->mbh;
-  //  mstar = data->mstar;
-
-  // replace z with generic name
-  if (m_r < gsl_histogram_min(ph.hist))
-    {
-      m_r = gsl_histogram_min(ph.hist);
-    }
-
-  if (m_r >= gsl_histogram_max(ph.hist))
-    {
-      m_r = (1. - 1.e-15) * gsl_histogram_max(ph.hist);
-    }
-
-
-  return m_r;
-}
 
 double val_func_1d (void *this_data, void *p)
 {
   //  (void)(params); /* avoid unused parameter warning */
 
-  struct bin_struct { gsl_histogram * hist; int column_index; };
+  struct bin_struct { gsl_histogram * hist; int icol; };
   bin_struct binfo = *(struct bin_struct *)p;
 
-  int column_index = binfo.column_index;
+  int icol = binfo.icol;
 
   
   struct ntuple_data * data = (struct ntuple_data *) this_data;
   double this_col_value;
 
-  this_col_value  = data->attributes[column_index];
+  this_col_value  = data->attributes[icol];
   //  z = data->attributes[1];
   //  mbh = data->mbh;
   //  mstar = data->mstar;
@@ -364,60 +336,43 @@ double val_func_1d (void *this_data, void *p)
 }
 
 
+double val_func_2d (void *this_data, void *p)
+{
+  //  (void)(params); /* avoid unused parameter warning */
+
+  struct bin_struct { gsl_histogram * hist; gsl_histogram * hist_v; int icol; int icol_v; int ibin_v;};
+  
+  struct ntuple_data * data = (struct ntuple_data *) this_data;
+  double this_col_data; //, v_data;
+
+  bin_struct ph = *(struct bin_struct *)p;
+
+  int icol = ph.icol;
+  this_col_data = data->attributes[icol];
+  //  v_data = data->attributes[ibin_v];
+  //  mbh = data->mbh;
+  //  mstar = data->mstar;
+
+  // replace z with generic name
+  if (this_col_data < gsl_histogram_min(ph.hist))
+    {
+      this_col_data = gsl_histogram_min(ph.hist);
+    }
+
+  if (this_col_data >= gsl_histogram_max(ph.hist))
+    {
+      this_col_data = (1. - 1.e-15) * gsl_histogram_max(ph.hist);
+    }
+
+
+  return this_col_data;
+}
+
+
 
 int Bin_Index_Hist2d(int i, int j,size_t nx,size_t ny)
 {
   return i * ny + j;
-
-}
-
-void Handle_Extreme_Values_Hist2d(gsl_histogram2d * hist2d, double x_value, double y_value)
-{
-
-  size_t i = 0,j = 0;
-  size_t dummy = 0;
-  size_t nx = hist2d->nx;
-  size_t ny = hist2d->ny;
-
-  if (x_value < gsl_histogram2d_xmin(hist2d))
-    {
-      i = 0;
-      //left_errors_x++;
-    }
-  else if (x_value < gsl_histogram2d_xmax(hist2d))
-    {
-      gsl_histogram2d_find(hist2d,x_value, gsl_histogram2d_ymin(hist2d),&i,&dummy);
-    }
-  if (x_value >= gsl_histogram2d_xmax(hist2d))
-    {
-      i = nx - 1;
-      //right_errors_x++;
-    }
-  else if (x_value >= gsl_histogram2d_xmin(hist2d))
-    {
-      gsl_histogram2d_find(hist2d,x_value, gsl_histogram2d_ymin(hist2d),&i,&dummy);
-    }
-	  
-  if (y_value < gsl_histogram2d_ymin(hist2d))
-    {
-      j = 0;
-      //left_errors_z++;
-    }
-  else if (y_value < gsl_histogram2d_ymax(hist2d))
-    {
-      gsl_histogram2d_find(hist2d,gsl_histogram2d_xmin(hist2d), y_value,&dummy,&j);
-    }
-  if (y_value >= gsl_histogram2d_ymax(hist2d))
-    {
-      j = ny - 1;
-      //right_errors_z++;
-    }
-  else if (y_value >= gsl_histogram2d_ymin(hist2d))
-    {
-      gsl_histogram2d_find(hist2d,gsl_histogram2d_xmin(hist2d), y_value,&dummy,&j);
-    }
-
-  hist2d->bin[Bin_Index_Hist2d(i,j,nx,ny)]++;
 
 }
 
