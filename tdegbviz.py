948068ea78cb68380f88ab97fccadf0162b23dab
Nathaniel Roth <nathanielroth1@gmail.com>
2020-04-02 14:53:32 -0400

    Changes to plotting histograms - trying to make it more modular

--- tdegbviz.py
+++ tdegbviz.py
@@ -2,25 +2,160 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-def plot_1d_histogram(prefix = './',hist_type = 'gals', axis = 'mstar_mendel'):
-    filename = prefix + hist_type + '_' + axis + '_1d.hist'
 
-    hist = np.loadtxt(filename)
-
-    left_edges = hist[::,0]
-    right_edges = hist[::,1]
-    bin_centers = 0.5 * (left_edges + right_edges) # assumes uniform spacing
-    widths = right_edges - left_edges
-    counts = hist[::,2]
-
-    print("Total counts is %f" %counts.sum())
-    plt.bar(bin_centers,counts, width = widths, align='center',edgecolor='k',color='white')
+class Binner:
 
 
+    def __init__(self,data_path = './',hist_type = 'gals',save_path = './'):
+
+        self.data_path = data_path
+        self.hist_type = hist_type
+        self.save_path = save_path
+
+    def set_data_path(self,data_path):
+
+        self.data_path = data_path
+        
+    def set_hist_type(self,hist_type):
+
+        self.hist_type = hist_type
+
+    def set_save_path(self,save_path):
+
+        self.save_path = save_path
 
 
-# could be combined with contour function (make another option?)
-def plot_2d_histogram(prefix ='./', hist_type = 'gals',axis1 = 'mstar_mendel', axis2 = 'UminusR', transpose_default = 0, cmap = "YlGnBu", cm_maxval = 1.0, cm_minval = 0., zlim = 0):
+    def plot_1d_histogram(self,axis):
+
+        self.axis = axis
+        self.working_filename = self.data_path + self.hist_type + '_' + self.axis + '_1d.hist'
+        self.savefile_name = self.working_filename.replace('.hist','.png')
+
+        hist = np.loadtxt(self.working_filename)
+
+        left_edges = hist[::,0]
+        right_edges = hist[::,1]
+        bin_centers = 0.5 * (left_edges + right_edges) # assumes uniform spacing
+        widths = right_edges - left_edges
+        counts = hist[::,2]
+
+        print("Total counts is %f" %counts.sum())
+        return plt.bar(bin_centers,counts, width = widths, align='center',edgecolor='k',color='white')
+
+    def save_current_histogram(self):
+
+        print("saving to ",self.savefile_name)
+        plt.savefig(self.savefile_name)
+
+    def read_2d_histogram(self,axis1='log_mbh_sigma',axis2 = 'z',transpose_default = 0):
+        try:
+            self.working_filename = self.data_path + self.hist_type + "_" + axis1 + "_" + axis2 + "_2d.hist"
+            self.savefile_name = self.working_filename.replace('.hist','.png')
+            self.hist_2d_counts = np.loadtxt(self.working_filename,skiprows=2,usecols=2)
+            self.switched_2d_name = 0
+            print("Read in data from", self.working_filename)
+        except:
+            try:
+                self.working_filename = self.data_path + self.hist_type + "_" + axis2 + "_" + axis1 + "_2d.hist"
+                self.savefile_name = self.working_filename.replace('.hist','.png')
+                self.hist_2d_counts = np.loadtxt(self.working_filename,skiprows=2,usecols=2)
+                self.switched_2d_name = 1
+                print("Read in data from ", self.working_filename)
+            except:
+                
+                print("Error: check names", self.working_filename)
+    
+        self.transpose_default = transpose_default
+
+        if self.switched_2d_name == 0:
+            if self.transpose_default == 0:
+                plt.xlabel(axis2,fontsize=16)                                
+                plt.ylabel(axis1,fontsize=16)   
+            else:
+                plt.xlabel(axis1,fontsize=16)                                
+                plt.ylabel(axis2,fontsize=16)  
+        else:
+            if self.transpose_default == 0:
+                plt.xlabel(axis1,fontsize=16)                                
+                plt.ylabel(axis2,fontsize=16)   
+            else:
+                plt.xlabel(axis2,fontsize=16)                                
+                plt.ylabel(axis1,fontsize=16)  
+
+
+
+    def plot_2d_histogram(self, zlim = 0, cmap_name = 'jet',cm_maxval = 1.,cm_minval = 0.):
+
+
+        h_grid = np.loadtxt(self.working_filename,max_rows=1) # edges
+        v_grid = np.loadtxt(self.working_filename,skiprows=1, max_rows=1) #edges
+    
+        hist_2d_counts = self.hist_2d_counts.reshape((len(h_grid) - 1,len(v_grid) - 1)) # centers
+
+        h_mesh,v_mesh = np.meshgrid(h_grid,v_grid,indexing='ij')
+
+        cmap = plt.get_cmap(cmap_name)
+        new_cmap = truncate_colormap(cmap,  maxval = cm_maxval, minval=cm_minval,)
+
+        if (self.transpose_default == 0):
+            hist2d = plt.pcolormesh(h_mesh,v_mesh, hist_2d_counts,shading='flat',cmap=new_cmap)  
+        else:
+            hist2d = plt.pcolormesh(v_mesh,h_mesh, hist_2d_counts,shading='flat',cmap=new_cmap)
+
+
+        cbar = plt.colorbar()                                                                          
+        max_count = np.amax(hist_2d_counts)                                                       
+        min_count = np.amin(hist_2d_counts)  
+
+        if (zlim == 0):
+            zlim = max_count
+    
+
+        cbar.ax.tick_params(labelsize=18)                                                          
+        cbar.set_label(r'rate',fontsize=16)  
+        plt.clim(0,zlim)
+                                                                                           
+
+        print("Total counts is %f" % hist_2d_counts.sum())
+
+        return hist2d, cbar
+
+    def plot_2d_contours(self, level_list = [0.1,0.3,0.5,0.7,0.9]):
+
+        h_grid = np.loadtxt(self.working_filename,max_rows=1) # edges
+        v_grid = np.loadtxt(self.working_filename,skiprows=1, max_rows=1) #edges
+
+
+        # contours want bin-centers, not bin-edges like pcolormesh
+        h_grid = np.array([0.5 * (h_grid[i] + h_grid[i+1]) for i in range(len(h_grid) - 1)])
+        v_grid = np.array([0.5 * (v_grid[i] + v_grid[i+1]) for i in range(len(v_grid) - 1)])
+    
+        hist_2d_counts = self.hist_2d_counts.reshape((len(h_grid),len(v_grid))) # centers
+
+        h_mesh,v_mesh = np.meshgrid(h_grid,v_grid,indexing='ij')
+
+        max_count = np.amax(hist_2d_counts)                                                      
+        min_count = np.amin(hist_2d_counts)
+        #contour_levels = np.array([0.1 * max_count,0.3 * max_count,0.5 * max_count, 0.7 * max_count,  0.9 * max_count])
+        contour_levels = max_count * np.array(level_list)
+
+        if (self.transpose_default == 0):
+            cont = plt.gca().contour(h_mesh,v_mesh,hist_2d_counts,contour_levels,colors='k',linewidths=2)
+        else:
+            cont = plt.gca().contour(v_mesh,h_mesh,hist_2d_counts,contour_levels,colors='k',linewidths=2)
+
+        print("Total counts is %f" % hist_2d_counts.sum())
+
+        return cont
+        
+# static
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+    new_cmap = matplotlib.colors.LinearSegmentedColormap.from_list('trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),cmap(np.linspace(minval, maxval, n)))
+    return new_cmap 
+
+
+# old way
+def plot_2d_histogram(prefix ='./', hist_type = 'gals',axis1 = 'log_mbh_sigma', axis2 = 'z', transpose_default = 0, cmap = "YlGnBu", cm_maxval = 1.0, cm_minval = 0., zlim = 0):
 
     switched_name = 0
     try:
@@ -38,7 +173,7 @@ def plot_2d_histogram(prefix ='./', hist_type = 'gals',axis1 = 'mstar_mendel', a
     h_grid = np.loadtxt(filename,max_rows=1) # edges
     v_grid = np.loadtxt(filename,skiprows=1, max_rows=1) #edges
     
-    hist_2d_counts = hist_2d_counts.reshape((len(h_grid) - 1,len(v_grid) - 1)) # centersw
+    hist_2d_counts = hist_2d_counts.reshape((len(h_grid) - 1,len(v_grid) - 1)) # centers
 
     h_mesh,v_mesh = np.meshgrid(h_grid,v_grid,indexing='ij')
         
@@ -46,9 +181,9 @@ def plot_2d_histogram(prefix ='./', hist_type = 'gals',axis1 = 'mstar_mendel', a
     new_cmap = truncate_colormap(cmap,  maxval = cm_maxval, minval=cm_minval,)
 
     if (transpose_default == 0):
-        plt.pcolormesh(h_mesh,v_mesh, hist_2d_counts,shading='gauraud',cmap=new_cmap)  
+        plt.pcolormesh(h_mesh,v_mesh, hist_2d_counts,shading='flat',cmap=new_cmap)  
     else:
-        plt.pcolormesh(v_mesh,h_mesh, hist_2d_counts,shading='gauraud',cmap=new_cmap) 
+        plt.pcolormesh(v_mesh,h_mesh, hist_2d_counts,shading='flat',cmap=new_cmap) 
 
     # need to figure out where to do this when plotting multiple things
     if switched_name == 0:
@@ -77,14 +212,14 @@ def plot_2d_histogram(prefix ='./', hist_type = 'gals',axis1 = 'mstar_mendel', a
 
 
     cbar.ax.tick_params(labelsize=18)                                                          
-    cbar.set_label(r'log density (code units)',fontsize=16)  
+    cbar.set_label(r'rate',fontsize=16)  
     plt.clim(0,zlim)
                                                                                            
 
     print("Total counts is %d" % hist_2d_counts.sum())
 
 
-
+# old way
 def plot_2d_contours(prefix ='./', hist_type = 'gals',axis1 = 'mstar_mendel', axis2 = 'UminusR', transpose_default = 0, cmap = "YlGnBu", cm_maxval = 1.0, cm_minval = 0., zlim = 0):
 
     switched_name = 0
@@ -116,15 +251,11 @@ def plot_2d_contours(prefix ='./', hist_type = 'gals',axis1 = 'mstar_mendel', ax
     contour_levels = np.array([0.1 * max_count,0.3 * max_count,0.5 * max_count, 0.7 * max_count,  0.9 * max_count])
 
     if (transpose_default == 0):
-        plt.contour(h_mesh,v_mesh,hist_2d_counts,contour_levels,colors='w',linewidths=3)
+        plt.contour(h_mesh,v_mesh,hist_2d_counts,contour_levels,colors='k',linewidths=2)
     else:
-        plt.contour(v_mesh,h_mesh,hist_2d_counts,contour_levels,colors='w',linewidths=3)
+        plt.contour(v_mesh,h_mesh,hist_2d_counts,contour_levels,colors='k',linewidths=2)
 
     print("Total counts is %f" % hist_2d_counts.sum())
 
 
-def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
-    new_cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
-        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
-        cmap(np.linspace(minval, maxval, n)))
-    return new_cmap 
+
